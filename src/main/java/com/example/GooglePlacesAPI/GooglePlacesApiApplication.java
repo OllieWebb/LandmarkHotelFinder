1445f8fedb4501d373bae42b763fd3c96b76d55e
OllieWebb <oliver.chester@hotmail.co.uk>
2024-07-26 13:56:04 +0100

    add project files

---
+++ src/main/java/com/example/GooglePlacesAPI/GooglePlacesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.GooglePlacesAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglePlacesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglePlacesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GooglePlacesAPI/GooglePlacesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GooglePlacesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooglePlacesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
