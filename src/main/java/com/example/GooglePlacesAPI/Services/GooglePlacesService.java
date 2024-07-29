08f0e38de9983221c7af86734a525c2379a0ab06
OllieWebb <oliver.chester@hotmail.co.uk>
2024-07-29 14:26:03 +0100

    Updated the unitilised google API and customised the returned fields. Additionally refactored the code and moved the functionality to service layer

--- src/main/java/com/example/GooglePlacesAPI/Services/GooglePlacesService.java
+++ src/main/java/com/example/GooglePlacesAPI/Services/GooglePlacesService.java
@@ -3,20 +3,58 @@ package com.example.GooglePlacesAPI.Services;
 import com.example.GooglePlacesAPI.HotelsModel.HotelResponse;
 import com.example.GooglePlacesAPI.LandmarkModel.LandmarkResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
 import java.io.IOException;
+import java.util.Collections;
 
 @Service
 public class GooglePlacesService {
 
-    public LandmarkResponse parseLandmarkResponse(String jsonResponse) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
+    @Value("${google.places.api.key}")
+    private String apiKey;
+
+    private RestTemplate restTemplate = new RestTemplate();
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    public LandmarkResponse getLandmarks(String text) throws IOException {
+        String urlLandmark = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + text + "&key=" + apiKey;
+        String jsonResponseLandmark = restTemplate.getForObject(urlLandmark, String.class);
+        return parseLandmarkResponse(jsonResponseLandmark);
+    }
+
+    public String getNearbyHotels(double lat, double lng, int radius) throws IOException {
+        String urlNearbySearch = "https://places.googleapis.com/v1/places:searchNearby";
+        String requestBody = String.format(
+                "{\"includedTypes\": [\"hotel\"], \"maxResultCount\": 10, \"locationRestriction\": {\"circle\": {\"center\": {\"latitude\": %f, \"longitude\": %f}, \"radius\": %f}}}",
+                lat, lng, (double) radius
+        );
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.set("X-Goog-Api-Key", apiKey);
+        headers.set("X-Goog-FieldMask", "places.displayName,places.rating,places.websiteUri,places.reviews,places.internationalPhoneNumber,places.formattedAddress");
+
+        HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
+
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(urlNearbySearch, HttpMethod.POST, entity, String.class);
+            return response.getBody();
+        } catch (HttpClientErrorException e) {
+            throw e;
+        }
+    }
+
+    private LandmarkResponse parseLandmarkResponse(String jsonResponse) throws IOException {
         return objectMapper.readValue(jsonResponse, LandmarkResponse.class);
     }
 
-    public HotelResponse parseHotelResponse(String jsonResponse) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
+    private HotelResponse parseHotelResponse(String jsonResponse) throws IOException {
         return objectMapper.readValue(jsonResponse, HotelResponse.class);
     }
-
 }
--- src/main/java/com/example/GooglePlacesAPI/controller/GooglePlacesController.java
+++ src/main/java/com/example/GooglePlacesAPI/controller/GooglePlacesController.java
@@ -2,64 +2,35 @@ package com.example.GooglePlacesAPI.controller;
 
 import com.example.GooglePlacesAPI.LandmarkModel.LandmarkResponse;
 import com.example.GooglePlacesAPI.Services.GooglePlacesService;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
 
 import java.io.IOException;
-import java.util.Collections;
 
 @RestController
 public class GooglePlacesController {
 
-    @Value("${google.places.api.key}")
-    private String apiKey;
+    @Autowired
+    private GooglePlacesService googlePlacesService;
 
-    private GooglePlacesService googlePlacesService = new GooglePlacesService();
-
-    @Operation(summary = "Get places based on location and radius")
-    @GetMapping("/testing")
-    public String getLandmark(
-            @Parameter(description = "text for searching") @RequestParam String text,
-            @Parameter(description = "Radius in meters") @RequestParam int radius
+    @GetMapping("/hotel")
+    public String getHotels(
+            @RequestParam String text,
+            @RequestParam int radius
     ) throws IOException {
-        String urlLandmark = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + text + "&key="+apiKey;
-        RestTemplate restTemplateLandmark = new RestTemplate();
-        String jsonResponseLandmark = restTemplateLandmark.getForObject(urlLandmark, String.class);
 
-        LandmarkResponse landmarkResponse = googlePlacesService.parseLandmarkResponse(jsonResponseLandmark);
+        LandmarkResponse landmarkResponse = googlePlacesService.getLandmarks(text);
+
+        if (landmarkResponse.getResults().isEmpty()) {
+            return "No results found.";
+        }
 
         double lat = landmarkResponse.getResults().get(0).getGeometry().getLocation().getLat();
         double lng = landmarkResponse.getResults().get(0).getGeometry().getLocation().getLng();
 
-        String urlHotel = "https://places.googleapis.com/v1/places:searchNearby?fields=*";
-
-        // Create request body
-        String requestBody = String.format(
-                "{\"includedTypes\": [\"restaurant\"], \"locationRestriction\": {\"circle\": {\"center\": {\"latitude\": %f, \"longitude\": %f}, \"radius\": %d}}}",
-                lat, lng, radius
-        );
-
-        // Set headers
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.set("Authorization", "Bearer " + apiKey);
-
-        // Create entity with headers and body
-        HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
-
-        RestTemplate restTemplateHotel = new RestTemplate();
-        ResponseEntity<String> response = restTemplateHotel.exchange(urlHotel, HttpMethod.POST, entity, String.class);
-        return response.getBody();
+        return googlePlacesService.getNearbyHotels(lat, lng, radius);
     }
 }
