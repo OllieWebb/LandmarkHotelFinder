7110b52ead91193c95854e99cf9ff21515a77330
Owen Graham <owen.graham@outlook.com>
2024-08-01 10:24:43 +0100

    removed availability check function to push landmark-hotel association to main

--- src/main/java/com/example/GooglePlacesAPI/controller/GooglePlacesController.java
+++ src/main/java/com/example/GooglePlacesAPI/controller/GooglePlacesController.java
@@ -2,12 +2,9 @@ package com.example.GooglePlacesAPI.controller;
 
 import com.example.GooglePlacesAPI.LandmarkModel.LandmarkResponse;
 import com.example.GooglePlacesAPI.Services.GooglePlacesService;
-import com.example.GooglePlacesAPI.Services.SeleniumService;
 import com.example.GooglePlacesAPI.exceptions.InvalidLandmarkException;
 import com.example.GooglePlacesAPI.exceptions.InvalidRadiusException;
 import com.example.GooglePlacesAPI.exceptions.NoResultsFoundException;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,9 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 public class GooglePlacesController {
@@ -25,8 +19,6 @@ public class GooglePlacesController {
     @Autowired
     private GooglePlacesService googlePlacesService;
 
-    @Autowired
-    private SeleniumService seleniumService;
 
     @GetMapping("/hotel")
     public String getHotels(
@@ -48,30 +40,5 @@ public class GooglePlacesController {
         double lng = landmarkResponse.getResults().get(0).getGeometry().getLocation().getLng();
 
         return googlePlacesService.getNearbyHotels(lat, lng, radius);
-    }
-
-    @Operation(summary = "Get availability of hotels based on location and radius")
-    @GetMapping("/available")
-    public Map<String, Boolean> getAvailability(
-            @Parameter(description = "text for searching") @RequestParam String text,
-            @Parameter(description = "Radius in meters") @RequestParam int radius,
-            @Parameter(description = "Check in date YYYY-MM-DD format") @RequestParam String checkInDate,
-            @Parameter(description = "Check out date YYYY-MM-DD format") @RequestParam String checkOutDate
-
-    ) throws IOException {
-        LandmarkResponse landmarkResponse = googlePlacesService.getLandmarks(text);
-        if (landmarkResponse.getResults().isEmpty()) {
-            return new HashMap<>();
-        }
-
-        double lat = landmarkResponse.getResults().get(0).getGeometry().getLocation().getLat();
-        double lng = landmarkResponse.getResults().get(0).getGeometry().getLocation().getLng();
-        List<String> hotelNames = googlePlacesService.getNearbyHotelNames(lat, lng, radius);
-        Map<String, Boolean> availabilityMap = new HashMap<>();
-        for (String hotel : hotelNames){
-            Map<String, Boolean> hotelAvailability = seleniumService.getHotelAvailability(hotel, checkInDate, checkOutDate);
-            availabilityMap.putAll(hotelAvailability);
-        }
-        return availabilityMap;
     }
 }
